76ccbf0088843d403c3012f9b4aa6afaa8cf914c
Danny Raetzsch <danny@livingasone.com>
2022-03-21 08:12:36 -0500

    Add some simple error handling

--- src/components/ContractSearch.vue
+++ src/components/ContractSearch.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        <input v-model="address" @input="timestamp=null" placeholder="contract address" />
-        <button @click="search(address)">Search Contracts</button>
+        <input v-model="address" @input="reset" v-on:keyup.enter="search" placeholder="contract address" />
+        <button @click="search">Search Contracts</button>
+    </div>
+    <div v-if="isLoading">
+        <span>Loading...</span>
     </div>
     <div v-if="timestamp != null">
         <label>Contract Timestamp:</label>
         <span>{{timestamp}}</span>
     </div>
+    <ul v-if="errors != null">
+        <li v-for="error in errors" :key="error.message">
+            <label>Error:</label>
+            <span>{{error.message}}</span>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -16,12 +25,22 @@ export default {
       return {
           address: '',
           timestamp: null,
+          isLoading: false,
+          errors: null,
       }
   },
   methods: {
-    async search(address) {
+    reset() {
+        this.isLoading = false;
+        this.errors = null;
+    },
+
+    async search() {
+        this.errors = null;
+        this.isLoading = true;
+
         // Poor-man's GraphQL query. Using Apollo here would be much cleaner.
-        const body = { "query":`query { contract(address: "${address}") { createdInTransaction { timestamp } } }` };
+        const body = { "query":`query { contract(address: "${this.address}") { createdInTransaction { timestamp } } }` };
         const response = await fetch('http://localhost:4000', {
             method: 'POST',
             headers: {
@@ -30,7 +49,14 @@ export default {
             body: JSON.stringify(body),
         });
 
+        this.isLoading = false;
+
         const responseBody = await response.json();
+
+        if (responseBody.errors) {
+            this.errors = responseBody.errors;
+        }
+
         if (responseBody.data && responseBody.data.contract) {
             this.timestamp = responseBody.data.contract.createdInTransaction.timestamp;
         }
